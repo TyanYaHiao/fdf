ad53630f095d35632f170b8e24776b2115afdbec
fsmith <tyanyahiao@gmail.com>
2019-03-10 18:22:58 +0300

    pixel

--- src/main.c
+++ src/main.c
@@ -6,14 +6,20 @@
 /*   By: fsmith <fsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/10 15:14:57 by fsmith            #+#    #+#             */
-/*   Updated: 2019/03/10 16:22:26 by fsmith           ###   ########.fr       */
+/*   Updated: 2019/03/10 17:44:48 by fsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-int				main(int argc, char **argv)
+int				main()
 {
-	ft_putstr("HELLO WORRRLD!");
+	void	*mlx_ptr;
+	void	*win_ptr;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
+	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
+	mlx_loop(mlx_ptr);
 	return (0);
 }
