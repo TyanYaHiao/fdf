953f66e8041543ee81448b3f28302ce79ca34387
fsmith <tyanyahiao@gmail.com>
2019-03-16 20:30:04 +0300

    2 lines with xyus

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fsmith <fsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/10 15:14:57 by fsmith            #+#    #+#             */
-/*   Updated: 2019/03/11 16:58:49 by mlurker          ###   ########.fr       */
+/*   Updated: 2019/03/16 20:28:30 by fsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,133 +22,159 @@ int		ft_abs1(int x1, int x0)
 	return (res * (-1));
 }
 
-void	print_vertical(t_map map)
+t_map	swap(t_map map)
 {
-	int 	delx = ft_abs1(map.x1, map.x0);
-	int 	dely = ft_abs1(map.y1, map.y0);
-	int i = 0;
+	int x = (map).x1;
+	int y = (map).y1;
+	(map).x1 = (map).x0;
+	(map).y1 = (map).y0;
+	(map).x0 = x;
+	(map).y0 = y;
+	return(map);
+}
 
-	while (i <= 2 * dely)
+void 	plot_line_low(t_map *map, int i, int check)
+{
+	if (check)
+		map[i] = swap(map[i]);
+	map[i].delx = ft_abs1(map[i].x1, map[i].x0);
+	map[i].dely = ft_abs1(map[i].y1, map[i].y0);
+	int D;
+	int y;
+	int yi = 1;
+	if (map[i].delx < 0)
 	{
-		if (map.y0 <= map.y1)
+		yi = -1;
+		map[i].delx *= -1;
+	}
+	D = 2 * map[i].dely - map[i].delx;
+	while (map[i].x0 < map[i].x1)
+	{
+
+		mlx_string_put(map[0].mlx_ptr, map[0].win_ptr, 200, 200, 0xcdcd, "Xyu");
+		mlx_pixel_put(map[0].mlx_ptr, map[0].win_ptr, map[i].x0, map[i].y0, 0x6a5acd);
+		if (D > 0)
 		{
-			mlx_pixel_put(map.mlx_ptr, map.win_ptr, map.x0, map.y0, 0xFFFFFF);
-			i++;
-			map.y0++;
+			map[i].y0 += yi;
+			D -= 2 * map[i].delx;
 		}
-		else if (map.y0 >= map.y1)
-		{
-			mlx_pixel_put(map.mlx_ptr, map.win_ptr, map.x1, map.y1, 0xFFFFFF);
-			i++;
-			map.y1--;
-		}
+		D += 2 * map[i].dely;
+		map[i].x0++;
 	}
 }
 
-void	print_horisontal(t_map map)
-{
-	int 	delx = ft_abs1(map.x1, map.x0);
-	int i = 0;
 
-	mlx_pixel_put(map.mlx_ptr, map.win_ptr, map.x1, map.y1, 0xFFFFFF);
-	while (i <= delx * 2)
+void 	plot_line_high(t_map *map, int i, int check)
+{
+	if (check)
+		map[i] = swap(map[i]);
+	map[i].delx = ft_abs1(map[i].x1, map[i].x0);
+	map[i].dely = ft_abs1(map[i].y1, map[i].y0);
+	int D;
+	int x;
+	int xi = 1;
+	if (map[i].delx < 0)
 	{
-		if (map.x0 <= map.x1)
+		xi = -1;
+		map[i].delx *= -1;
+	}
+	D = 2 * map[i].delx - map[i].dely;
+	mlx_pixel_put(map[0].mlx_ptr, map[0].win_ptr, map[i].x0, map[i].y0, 0xcdcd);
+	mlx_pixel_put(map[0].mlx_ptr, map[0].win_ptr, map[i].x1, map[i].y1, 0x6a5acd);
+	while (map[i].y0 < map[i].y1)
+	{
+
+		mlx_string_put(map[0].mlx_ptr, map[0].win_ptr, 300, 300, 0xcdcd, "Xyu4");
+		mlx_pixel_put(map[0].mlx_ptr, map[0].win_ptr, map[i].x0, map[i].y0, 0x6a5acd);
+		if (D > 0)
 		{
-			mlx_pixel_put(map.mlx_ptr, map.win_ptr, map.x0, map.y0, 0xFFFFFF);
-			i++;
-			map.x0++;
+			map[i].x0 += xi;
+			D -= 2 * map[i].dely;
+		}
+		D += 2 * map[i].delx;
+		map[i].y0++;
+	}
+}
+
+void	plot_line(t_map *map, int i)
+{
+
+	if (map[i].dely < map[i].delx)
+	{
+		mlx_string_put(map[0].mlx_ptr, map[0].win_ptr, 200, 200, 0xcdcd, "Xyu3");
+		if (map[i].x0 > map[i].x1)
+			plot_line_low(map, i, 1);
+		else
+			plot_line_low(map, i, 0);
+	}
+	else
+	{
+		if (map[i].y0 > map[i].y1)
+		{
+			plot_line_high(map, i, 1);
+			mlx_string_put(map[0].mlx_ptr, map[0].win_ptr, 200, 200, 0xcdcd, "Xyu1");
 		}
 		else
 		{
-			mlx_pixel_put(map.mlx_ptr, map.win_ptr, map.x1, map.y1, 0xFFFFFF);
-			i++;
-			map.x1--;
+			plot_line_high(map, i, 0);
+			mlx_string_put(map[0].mlx_ptr, map[0].win_ptr, 500, 200, 0xcdcd, "Xyu2");
 		}
 	}
 }
 
-void 	print_low(t_map map)
-{
-	int i = 1;
-	int Dxy = 2 *(map.delx - map.dely);
-	int y = map.y0;
-
-//	while (i <= map.delx && )
-	{
-		if (map.x0 < map.x1)
-		{
-			mlx_pixel_put(map.mlx_ptr, map.win_ptr, map.x0, map.y0, 0xFFFFFF);
-
-		}
-
-	}
-}
-
-void 	plot_line_low(t_map map)
-{
-	int i = 0;
-	int 	x = map.x1;
-
-	while (i < map.delx)
-	{
-		if (map.x0 < map.x1)
-		{
-			mlx_pixel_put(map.mlx_ptr, map.win_ptr, map.x0, map.y0, 0xFFFFFF);
-			map.x0++;
-		}
-		else
-		{
-			mlx_pixel_put(map.mlx_ptr, map.win_ptr, x, map.y1, 0xFFFFFF);
-			x--;
-		}
-		i++;
-	}
-}
+//void	plot_line(t_map *map, int i)
+//{
+//	map[i].delx = ft_abs1(map[i].x1, map[i].x0);
+//	map[i].dely = ft_abs1(map[i].y1, map[i].y0);
+//	double 	err = 0;
+//	int 	delerr = map[i].dely / map[i].delx;
+//	int 	diry = 1;
+//	if (map[i].dely < 0)
+//		diry = -1;
+//
+//	while (map[i].x0 < map[i].x1)
+//	{
+//		mlx_string_put(map.mlx_ptr, map.win_ptr, 100, 200, 0xfaebd7, "Xyu");
+//		mlx_pixel_put(map[0].mlx_ptr, map[0].win_ptr, map[i].x0, map[i].y0, 0x6a5acd);
+//		err += delerr;
+//		if (err >= 0.5)
+//		{
+//			map[i].y0 += diry;
+//			err -= 1;
+//		}
+//		map[i].x0++;
+//	}
+//}
 
 int				main()
 {
 	t_map	*map;
-	char 	*str = "Window";
+	char 	*str = "Sosatb";
 	int 	k[2] = {70, 75};
 	int 	j[2] = {70, 75};
-	int i = -1;
+	int i = 0;
 	map = (t_map*)malloc(sizeof(*map) * 10);
 	map[0].x0 = 90;
 	map[0].x1 = 200;
 	map[0].y0 = 90;
 	map[0].y1 = 200;
-	map[1].x1 = 90;
-	map[1].x0 = 200;
-	map[1].y1 = 90;
+	map[1].x1 = 200;
+	map[1].x0 = 400;
+	map[1].y1 = 400;
 	map[1].y0 = 200;
 
+	map[0].mlx_ptr = mlx_init(); //нужно
+	map[0].win_ptr = mlx_new_window(map[0].mlx_ptr, 900, 900, "Lejatb"); //создает окно
+	mlx_string_put(map[0].mlx_ptr, map[0].win_ptr, 100, 200, 0xfaebd7, str);
 
-	map[0].mlx_ptr = mlx_init();
-	map[0].win_ptr = mlx_new_window(map[0].mlx_ptr, 500, 500, "mlx 42");
-	map[0].img_ptr = mlx_new_image(map[0].mlx_ptr, 100, 100);
-	mlx_put_image_to_window(map[0].mlx_ptr, map[0].win_ptr, map[0].img_ptr, 100, 100);
-	mlx_xpm_file_to_image(map[0].mlx_ptr, "0 0 0 0 1 1 1 2 2 2 2", k, j);
-	mlx_string_put(map[0].mlx_ptr, map[0].win_ptr, 100, 100, 0xfaebd7, str);
+	mlx_pixel_put(map[0].mlx_ptr, map[0].win_ptr, map[1].x1, map[1].y1, 0xFFFFFF); //set pixel
 
+	while (i < 2)
+	{
+		plot_line(map, i);
+		i++;
+	}
 
-	double 	err = 0;
-//	int 	delerr = dely / delx;
-	int 	diry = 1;
-
-	mlx_pixel_put(map[0].mlx_ptr, map[0].win_ptr, map[0].x1, map[0].y1, 0xFFFFFF);
-//	print_low(map);
-//	print_horisontal(map);
-//	print_vertical(map);
-
-	map[0].delx = ft_abs1(map[0].x1, map[0].x0);
-	map[0].dely = ft_abs1(map[0].y1, map[0].y0);
-
-	while (i++ < 2)
-		plot_line_low(map[0]);
-//	else
-//		plot_line_high();
-
-	mlx_loop(map[0].mlx_ptr);
+	mlx_loop(map[0].mlx_ptr); //тоже нужно
 	return (0);
 }
